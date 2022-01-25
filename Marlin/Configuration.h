14472ad5cc16e52be36dbe672bb73e42569d923b
Robert <robert@pepzi.org>
2022-01-25 20:24:47 +0100

    revert y_min_pos (i don't understand!)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -863,7 +863,7 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 4
-#define Y_MIN_POS 25
+#define Y_MIN_POS 7
 #define Z_MIN_POS 0
 #define X_MAX_POS (X_BED_SIZE + X_MIN_POS) + 15 // Extended max to allow the probe to reach more of the bed.
 #define Y_MAX_POS (Y_BED_SIZE + Y_MIN_POS)
