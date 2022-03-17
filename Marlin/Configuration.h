5e1fa4d4a87d9a448926ea51e1cf801e28d4af21
Robert <robert@pepzi.org>
2022-03-17 23:33:38 +0100

    Change nozzle to probe offsets for direct drive assembly.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -707,7 +707,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -47, -7, -0.74 }
+#define NOZZLE_TO_PROBE_OFFSET { -45, -6, -0.95 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
