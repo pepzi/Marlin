871754d87101410ce8699b26f67dccdedb170051
Robert <robert@pepzi.org>
2022-03-18 00:34:39 +0100

    Set max extrusion length to 200 (no longer using bowden)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -719,7 +719,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 1000
+#define EXTRUDE_MAXLENGTH 200
 
 //===========================================================================
 //======================== Thermal Runaway Protection =======================
