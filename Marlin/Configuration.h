a5f7ac770ee2efac9df804b5aade58b42d586d5c
Robert <robert@pepzi.org>
2022-03-20 19:40:15 +0100

    PIDs for direct drive

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -602,9 +602,9 @@
     #define DEFAULT_Kd_LIST {  78.81,  78.81 }
   #else
     // Autotuned using M303 E0 C8 S210
-    #define DEFAULT_Kp 24.45
-    #define DEFAULT_Ki 2.28
-    #define DEFAULT_Kd 65.52
+    #define DEFAULT_Kp 25.03
+    #define DEFAULT_Ki 2.16
+    #define DEFAULT_Kd 72.60
   #endif
 #endif // PIDTEMP
 
@@ -643,9 +643,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 201.01
-  #define DEFAULT_bedKi 39.26
-  #define DEFAULT_bedKd 686.10
+  #define DEFAULT_bedKp 160.54
+  #define DEFAULT_bedKi 27.49
+  #define DEFAULT_bedKd 625.03
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
